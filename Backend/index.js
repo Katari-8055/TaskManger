3513a0e4933cd3dd1a4dd64164d2b7b27a39a7ca
Katari <143288488+Katari-8055@users.noreply.github.com>
2025-05-26 14:32:06 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: "https://taskmanger-frontend-9atm.onrender.com",  // frontend URL
+  origin: [
+    "http://localhost:5173",
+    "https://taskmanger-frontend.onrender.com" // 👉 तेरे फ्रंटेंड का Render वाला लिंक
+  ],
   credentials: true,
 }));
 app.use(express.json());
